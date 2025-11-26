ab05c5a01ec932f53da9d1d3db9a3eead33bc3df
teja209 <tejasiva3118@gmail.com>
2025-11-26 23:07:06 +0530

    modified:   task_tracker.py

--- task_tracker.py
+++ task_tracker.py
@@ -4,18 +4,30 @@ how it works: runs the script, it greets in cli, it gives the user options to se
 the options are add, update, delete tasks; and mark the status of a task, and list the tasks
 '''
 import json
-db=[]
+import time
+db={}
+t=True
 with open("userdb.json", 'a') as db1:
     db=db1
-t=True
-print("hi what do you wanna do today\n")
 
 while t:
-    input_=input()
-    
-    if input_=="add":
-        task_name=input()
-
-
-    elif input_ == "end" or input_ == "\n":
-        break
+    input_=input("enter what you wanna do")
+    if input_.startswith("add"):
+        task=input_[4:]
+        db
+    elif input_.startswith("update"):
+        ...
+    elif input_.startswith("delete"):
+        ...
+    elif input_.startswith("list"):
+        ...
+    elif input_.startswith("list of done"):
+        ...
+    elif input_.startswith("list of todo"):
+        ...
+    elif input_.startswith("list of ongoing"):
+        ...
+    elif input_=="\n":
+        t=False
+    else:
+        print("invalid function enter proper function")
