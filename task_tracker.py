79ed4a61ae7680333f77d641814b416a10107547
teja209 <tejasiva3118@gmail.com>
2025-11-26 23:05:00 +0530

    modified:   task_tracker.py 	new file:   userdb.json

--- task_tracker.py
+++ task_tracker.py
@@ -1,7 +1,21 @@
 #  make a task tracker that works in cli
+'''
+how it works: runs the script, it greets in cli, it gives the user options to select from
+the options are add, update, delete tasks; and mark the status of a task, and list the tasks
+'''
+import json
+db=[]
+with open("userdb.json", 'a') as db1:
+    db=db1
+t=True
+print("hi what do you wanna do today\n")
+
+while t:
+    input_=input()
+    
+    if input_=="add":
+        task_name=input()
 
 
-def main():
-    ...
-
-def features(): 
+    elif input_ == "end" or input_ == "\n":
+        break
